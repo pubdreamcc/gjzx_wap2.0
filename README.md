f2ba68eae94299f6e8d0619f1984ce5b52f8ea0a
pubdreamcc <493310907@qq.com>
2019-04-09 18:08:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# lbxy-v1-wap
+
+> 本项目是国匠在线旗下鲁班学院移动端，用到的技术栈：vue+vuex+axios+vux(vue移动端ui框架)+flexible.js(移动端适配方案)+sass(css预处理技术)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="box1">我是box1</div>
+    <div class="box2">我是box2</div>
+    <div class="box3">我是box3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang='scss'>
+  body{
+    margin: 0;
+    padding: 0;
+  }
+  #app{
+    font-size: 30px;
+    background: pink;
+    height: 500px;
+    .box1{
+      width: 150px;
+      background: gray;
+    }
+    .box2{
+      width: 500px;
+      background: yellowgreen;
+    }
+    .box3{
+      width: 100px;
+      background: #00ccaa;
+    }
+    div{
+      float: left;
+    }
+  }
+</style>
